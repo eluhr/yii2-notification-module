ba1d47b83384f88617f8b7447bf60b356625ed3e
Elias Luhr <e.luhr@herzogkommunikation.de>
2020-01-21 14:28:50 +0100

    Fixed buggy migration

--- src/migrations/m190712_200215_add_permissions.php
+++ src/migrations/m190712_200215_add_permissions.php
@@ -21,18 +21,24 @@ VALUES
 	('notification.compose_a_message', 2, 'User is able to compose a message and send it to users', NULL, NULL, 1562961684, 1562961684),
 	('notification.send_mail_to_everyone', 2, 'User is able to send a mail to every user', NULL, NULL, 1562961618, 1562961618),
 	('notification.user_group', 2, 'User is able to maintain user groups and send messages to created user groups', NULL, NULL, 1562961660, 1562961660);
+SQL
+        );
 
+        $this->execute(<<<SQL
 INSERT INTO `{$authItemTableName}` (`name`, `type`, `description`, `rule_name`, `data`, `created_at`, `updated_at`)
 VALUES
 	('NotificationAdmin', 1, 'Full access to the notification module', NULL, NULL, 1562961827, 1562961827);
+SQL
+        );
 
+        $this->execute(<<<SQL
 INSERT INTO `{$authItemChildTableName}` (`parent`, `child`)
 VALUES
 	('NotificationAdmin', 'notification.compose_a_message'),
 	('NotificationAdmin', 'notification.send_mail_to_everyone'),
 	('NotificationAdmin', 'notification.user_group');
 SQL
-);
+        );
     }
 
     /**
--- src/migrations/m190713_071215_add_permissions.php
+++ src/migrations/m190713_071215_add_permissions.php
@@ -19,13 +19,15 @@ class m190713_071215_add_permissions extends Migration
 INSERT INTO `{$authItemTableName}` (`name`, `type`, `description`, `rule_name`, `data`, `created_at`, `updated_at`)
 VALUES
 	('notification.send_priority_mail', 2, 'User is able to send a prioritised message', NULL, NULL, 1562961684, 1562961684);
-
+SQL
+        );
+        $this->execute(<<<SQL
 
 INSERT INTO `{$authItemChildTableName}` (`parent`, `child`)
 VALUES
 	('NotificationAdmin', 'notification.send_priority_mail');
 SQL
-);
+        );
     }
 
     /**
